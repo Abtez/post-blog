55a2dbfce69770342400e3f8fd877ab6ad22be21
Abzed <ibraabzed@gmail.com>
2020-09-26 18:58:36 +0300

    add bio and profile pic

--- app/main/views.py
+++ app/main/views.py
@@ -57,6 +57,36 @@ def new_comment(blog_id):
         return redirect(url_for('main.index'))
     
     return render_template('comments.html', form=form, comment=comment, blog_id=blog_id)
+
+@main.route('/user/<uname>/bio',methods = ['GET','POST'])
+@login_required
+def update_bio(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    bioform = BioForm()
+
+    if bioform.validate_on_submit():
+        user.bio = bioform.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/bio.html',bioform=bioform)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
         
         
     
