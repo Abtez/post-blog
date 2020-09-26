ebe1dde006affd3f3e507a333393cb68f1b3ab57
Abzed <ibraabzed@gmail.com>
2020-09-26 17:38:31 +0300

    add blogs on the main page

--- app/main/views.py
+++ app/main/views.py
@@ -9,7 +9,8 @@ from .. import db,photos
 @main.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+    blogs = Blog.query.all()
+    return render_template('index.html',blogs=blogs)
 
 @main.route('/user/<uname>')
 def profile(uname):
@@ -21,3 +22,25 @@ def profile(uname):
         abort(404)
         
     return render_template("profile/profile.html", user = user, blog=blog)
+
+@main.route('/new_blog', methods=['GET','POST'])
+@login_required
+def new_blog():
+    form = BlogForm()
+    if form.validate_on_submit():
+        blog = form.blog.data
+        title = form.title.data
+        new_blog=Blog(blog=blog,title=title,user_id=current_user.id)
+        
+        new_blog.save_blog()
+        
+        return redirect(url_for('main.index'))
+    
+    return render_template('blogs.html', form=form)
+        
+        
+        
+    
+    
+        
+
