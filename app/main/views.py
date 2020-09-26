82bb2eeb627861081ce11dbea672d0e336c8ce62
Abzed <ibraabzed@gmail.com>
2020-09-26 14:27:58 +0300

    add bootstrap html and simplemde

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-from ..request import get_movies,get_movie,search_movie
+from ..request import get_quotes
 from .forms import BlogForm,Bioform, CommentForm
 from ..models import Blog,User, Comments
 from flask_login import login_required,current_user
